c36b6b1b1b86042be10bf9562b5bdb2fd77329ac
haoyundong <277079581@qq.com>
2018-03-10 19:40:08 +0800

    web test

---
+++ WebviewTestDemo/src/main/java/com/zhimazg/webviewtest/controller/WebviewActivity.java
@@ -0,0 +1,38 @@
+package com.zhimazg.webviewtest.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebviewActivity extends Activity {
+
+    private WebView webView;
+    private Activity mActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = this;
+
+        loadView();
+        loadData();
+    }
+
+    private void loadView() {
+        webView = new WebView(this);
+        setContentView(webView);
+    }
+
+    private void loadData() {
+        loadHtmlText();
+
+    }
+
+    /**
+     * 通过加载超文本来加载页面
+     */
+    private void loadHtmlText() {
+        String summary = "<html><body>You scored <b>192</b> points.</body></html>";
+        webView.loadData(summary, "text/html", "UTF-8");
+    }
+}
