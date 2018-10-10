7763033c04d27672b1e30178faddbbb38d2b04fe
haoyundong <277079581@qq.com>
2018-10-10 20:17:58 +0800

    adjust

--- PerformanceOptimize/src/main/java/com/sample/performanceoptimize/MainActivity.java
+++ PerformanceOptimize/src/main/java/com/sample/performanceoptimize/MainActivity.java
@@ -1,13 +1,43 @@
 package com.sample.performanceoptimize;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private final String[] data = {
+            "item1",
+            "item2"
+    } ;
+
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.lv_main);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (position) {
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+        }
     }
 }
