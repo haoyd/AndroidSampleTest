955e32d701d4853e182649bd48f9a6f824961af1
haoyundong <277079581@qq.com>
2018-10-24 14:45:11 +0800

    双开检测

--- PreventCheatDemo/src/main/java/com/sample/preventcheatdemo/MainActivity.java
+++ PreventCheatDemo/src/main/java/com/sample/preventcheatdemo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.sample.preventcheatdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.lahm.library.EasyProtectorLib;
+import com.lahm.library.VirtualApkCheckUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,9 +38,15 @@ public class MainActivity extends AppCompatActivity {
         boolean isMutiPkg = cheatManager.isMutiPkg();
         boolean isMutiVirtual = CheckVirtual.isRunInVirtual();
 
-        sb.append("dirCheckVirtual : " + !isFileDirReal + "\n");
-        sb.append("PkgCheckVitual : " + isMutiPkg + "\n");
-        sb.append("uidCheckVirtual : " + isMutiVirtual + "\n");
+//        sb.append("dirCheckVirtual : " + !isFileDirReal + "\n");
+//        sb.append("PkgCheckVitual : " + isMutiPkg + "\n");
+//        sb.append("uidCheckVirtual : " + isMutiVirtual + "\n");
+
+        sb.append((VirtualApkCheckUtil.getSingleInstance().checkByPrivateFilePath(this) ? "私有路径检测有多开" : "私有路径检测正常") + "\n");
+        sb.append((VirtualApkCheckUtil.getSingleInstance().checkByOriginApkPackageName(this) ? "包名检测有多开" : "包名检测正常") + "\n");
+        sb.append((VirtualApkCheckUtil.getSingleInstance().checkByMultiApkPackageName() ? "maps检测有多开" : "maps检测正常") + "\n");
+        sb.append((EasyProtectorLib.checkIsUsingMultiVirtualApp() ? "ps检测有多开" : "ps检测正常") + "\n");
+        sb.append((VirtualApkCheckUtil.getSingleInstance().checkByHasSameUid() ? "uid检测有多开" : "uid检测正常") + "\n");
     }
 
 
---
+++ PreventCheatDemo/src/main/java/com/sample/preventcheatdemo/MyApplication.java
@@ -0,0 +1,12 @@
+package com.sample.preventcheatdemo;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
