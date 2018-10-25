3f67db73b9c0cf46b77dde22e9fddfe2d322acdf
haoyundong <277079581@qq.com>
2018-10-25 10:23:34 +0800

    。。。

--- DebugDemo/src/main/java/com/sample/debugdemo/ProfilerTestActivity.java
+++ DebugDemo/src/main/java/com/sample/debugdemo/ProfilerTestActivity.java
@@ -2,6 +2,7 @@ package com.sample.debugdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfilerTestActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ProfilerTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiler_test);
+    }
+
+    public void click1(View view) {
+
     }
 }
